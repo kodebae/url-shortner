b8588a46a35f26d34e81124a431d790791a8c047
kodebae <heykarmendurbin@gmail.com>
2022-10-19 20:06:04 -0400

    feat: setting up main functionality of the app

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import tkinter
 
-root = tkinter.Tk() # creating a root window, our parent container for the UI
+root = tkinter.Tk() # creating a root window, our parent container for the UI. Every Tkinter widget must have a parent/root.
 root.title("Tiny URL Shortener") # our URL title
-root.geometry("350x150")
+root.geometry("350x150") # setting the size of the window, but can still resize the window manually. This will require a geometry manager. 
 
-root.mainloop()
+# main functionality of the app
+
+longurl_label = tkinter.Label(root, text="Enter Long URL") 
+longurl_entry = tkinter.Entry(root)
+shorturl_label = tkinter.Label(root, text="Your shortened URL")
+shorturl_entry = tkinter.Entry(root)
+shortenurl_button = tkinter.Button(root, text="Shorten URL")
+
+
+root.mainloop() # how Tkinter runs apps, the infinite loop runs while the app is running.
