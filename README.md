9ab5e180f8207f3d318957708cad5a68786057ce
kodebae <heykarmendurbin@gmail.com>
2023-03-02 12:56:05 -0500

    chore: debugging

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 ## Getting started with Tkinter
 
 Tkinter is a standard GUI library used for Python development.
-[Tkinter Docs](https://docs.python.org/3/library/tk.html)
+[Tkinter Docs](https://docs.python.org/3/library/tk.html
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+# from tkinter import *
 import tkinter
+import pyshorteners
+
+def shorten_url():
+    shortner = pyshortners.Shortener() # instance of Shorten class
+    short_url = shortner.tinyurl.short(longurl_entry.get()) # passing in long url, wrapper for tiny url API
+    print(shorturl_entry.insert(0, short_url))
 
 root = tkinter.Tk() # creating a root window, our parent container for the UI. Every Tkinter widget must have a parent/root.
 root.title("Tiny URL Shortener") # our URL title
-root.geometry("350x150") # setting the size of the window, but can still resize the window manually. This will require a geometry manager. 
+root.geometry("300x150") # setting the size of the window, but can still resize the window manually. This will require a geometry manager. 
+root.configure(background="red")
 
 # main functionality of the app
 
@@ -10,7 +18,16 @@ longurl_label = tkinter.Label(root, text="Enter Long URL")
 longurl_entry = tkinter.Entry(root)
 shorturl_label = tkinter.Label(root, text="Your shortened URL")
 shorturl_entry = tkinter.Entry(root)
-shortenurl_button = tkinter.Button(root, text="Shorten URL")
+shortenurl_button = tkinter.Button(root, text="Shorten URL", command=shorten_url)
 
+# placing widgets on the screen and centering them with pack geometry manager.
 
-root.mainloop() # how Tkinter runs apps, the infinite loop runs while the app is running.
+longurl_label.pack()
+longurl_entry.pack()
+shorturl_label.pack()
+shorturl_entry.pack()
+shortenurl_button.pack()
+
+# how Tkinter runs apps, the infinite loop runs while the app is running.
+
+root.mainloop() 
