76cf2311e2d23ebd039ecc096bfb958002d7983c
kodebae <heykarmendurbin@gmail.com>
2023-05-16 20:00:23 -0400

    docs: tkinter has some depreciation issues

--- main.py
+++ main.py
@@ -1,33 +1,51 @@
-# from tkinter import *
-import tkinter
-import pyshorteners
+import tkinter as tk
+import string
+import random
+
+def generate_short_url():
+    characters = string.ascii_letters + string.digits
+    short_url = ''.join(random.choices(characters, k=6))
+    return short_url
 
 def shorten_url():
-    shortner = pyshortners.Shortener() # instance of Shorten class
-    short_url = shortner.tinyurl.short(longurl_entry.get()) # passing in long url, wrapper for tiny url API
-    print(shorturl_entry.insert(0, short_url))
+    long_url = entry.get()
+    short_url = generate_short_url()
+    url_mapping[short_url] = long_url
+    result_label.config(text=f"Short URL: {short_url}", fg="green")
 
-root = tkinter.Tk() # creating a root window, our parent container for the UI. Every Tkinter widget must have a parent/root.
-root.title("Tiny URL Shortener") # our URL title
-root.geometry("300x150") # setting the size of the window, but can still resize the window manually. This will require a geometry manager. 
-root.configure(background="red")
+def redirect_url():
+    short_url = entry.get()
+    if short_url in url_mapping:
+        long_url = url_mapping[short_url]
+        result_label.config(text=f"Redirecting to: {long_url}", fg="blue")
+        # Add your redirection logic here
+    else:
+        result_label.config(text="Invalid URL", fg="red")
 
-# main functionality of the app
+url_mapping = {}
 
-longurl_label = tkinter.Label(root, text="Enter Long URL") 
-longurl_entry = tkinter.Entry(root)
-shorturl_label = tkinter.Label(root, text="Your shortened URL")
-shorturl_entry = tkinter.Entry(root)
-shortenurl_button = tkinter.Button(root, text="Shorten URL", command=shorten_url)
+# Create the main window
+window = tk.Tk()
+window.title("URL Shortener")
+window.configure(bg="#F0F0F0")
 
-# placing widgets on the screen and centering them with pack geometry manager.
+# Create GUI elements
+label = tk.Label(window, text="Enter a URL:", fg="black", bg="#F0F0F0")
+label.pack()
 
-longurl_label.pack()
-longurl_entry.pack()
-shorturl_label.pack()
-shorturl_entry.pack()
-shortenurl_button.pack()
+entry = tk.Entry(window, width=40)
+entry.pack()
 
-# how Tkinter runs apps, the infinite loop runs while the app is running.
+shorten_button = tk.Button(window, text="Shorten", command=shorten_url, fg="white", bg="green")
+shorten_button.pack()
 
-root.mainloop() 
+redirect_button = tk.Button(window, text="Redirect", command=redirect_url, fg="white", bg="blue")
+redirect_button.pack()
+
+result_label = tk.Label(window, text="", fg="black", bg="#F0F0F0")
+result_label.pack()
+
+# Start the Tkinter event loop
+window.mainloop()
+
+
